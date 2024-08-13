77ea382e38e5fed0bf43a340c30282e66c21f211
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-08-13 19:57:18 +0530

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-const myInput = document.getElementsByClassName('input');
+const myInput = document.querySelector('.input');
 console.log(myInput);
 
-let str = '0';
+let str = '0123';
+myInput.textContent = str;
 
-myInput[0].innerText = str;
+
+const reset = document.getElementById('reset');
+const equal = document.getElementById('equals');
+const del = document.getElementById('del');
+
+del.addEventListener('click' , ()=>{
+    if(str != '0'){
+        str = str.slice(0,str.length-1)
+    }  
+    myInput.textContent = str;
+})
+
+myInput.textContent = str;
+
+reset.addEventListener('click' ,() => {
+    myInput.textContent = 0;
+})
+
 
 
